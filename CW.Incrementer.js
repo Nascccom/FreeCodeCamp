955787fda150ff679236eebcaa1f519a5f5d279c
Nascccom <ostrov_o@bk.ru>
2022-10-26 15:54:59 +0300

    task for the incrementer

---
+++ CW.Incrementer.js
@@ -0,0 +1,31 @@
+/* Given an input of an array of digits, return the array with each digit incremented by its position in the array: the first digit will be incremented by 1, the second digit by 2, etc. Make sure to start counting your positions from 1 ( and not 0 ).
+
+Your result can only contain single digit numbers, so if adding a digit with its position gives you a multiple-digit number, only the last digit of the number should be returned.
+
+Notes:
+return an empty array if your array is empty
+arrays will only contain numbers so don't worry about checking that
+Examples:
+[1, 2, 3]  -->  [2, 4, 6]   #  [1+1, 2+2, 3+3]
+
+[4, 6, 9, 1, 3]  -->  [5, 8, 2, 5, 8]  #  [4+1, 6+2, 9+3, 1+4, 3+5]
+                                       #  9+3 = 12  -->  2 */
+
+function incrementer(nums) { 
+var arr =[];
+var is_num = 0;
+var str = '';
+var sum = 0;
+if (nums.length === 0) return arr;
+
+for (let i = 0; i < nums.length; i++) {
+    is_num = nums[i] + i + 1;
+    if (is_num > 9) {
+        str = String(is_num);
+        sum = +str[1];
+        arr.push(sum);
+    } else { 
+        arr.push(is_num);}
+}
+return arr
+}                                       
